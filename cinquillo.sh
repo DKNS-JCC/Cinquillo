b9433cd7b0a4526df52e2fbdeca8fa6aedc1cc31
DKNS-Usal <jorgecuadradojcc@usal.es>
2023-10-26 17:46:08 +0200

    Definitiva Config

--- cinquillo.sh
+++ cinquillo.sh
@@ -2,7 +2,6 @@
 
 function mostrarMenu
 {
-    clear
     echo "==============================="
     echo "          CINQUILLO           "
     echo "                    .------.  "
@@ -22,142 +21,112 @@ function mostrarMenu
     echo "S) SALIR"
     echo "==============================="
     echo -n "Introduzca una opcion.......... " # -n no hace salto de linea
-
-}
-
-function crearDefault
-{
-    echo "JUGADORES=2" > config.cfg
-    echo "ESTRATEGIA=0" >> config.cfg
-    echo LOG=./log/fichero.log >> config.cfg     
-    echo "Archivo de configuración creado correctamente, se recomienda modificar sus valores antes de jugar."
 }
 
 function configuracion
 {
-while true; 
-do
-    clear
-    echo "==============================="
-    echo "          CONFIGURACION        "
-    echo "==============================="
-    echo "1) NUMERO JUGADORES"
-    echo "2) SELECCIONAR ESTRATEGIA"
-    echo "3) SELECCIONAR LOG"
-    echo "5) VER CONFIGURACION"
-    echo "4) VOLVER"
-    echo "==============================="
-    echo -n "Introduzca una opción.......... " # -n no hace salto de linea
-    read opcion
+    while true; do
+        JUGADORES=$(grep '^JUGADORES=' config.cfg | cut -d '=' -f 2)
+        ESTRATEGIA=$(grep '^ESTRATEGIA=' config.cfg | cut -d '=' -f 2)
+        LOG=$(grep '^LOG=' config.cfg | cut -d '=' -f 2)
 
-    case $opcion in
-        1)
-            echo "JUGADORES actuales: $(grep '^JUGADORES=' config.cfg)"
-            echo -n "Nuevo número de jugadores (2, 3 o 4): "
-            read nuevo_jugadores
+        echo "JUGADORES: $JUGADORES"
+        echo "ESTRATEGIA: $ESTRATEGIA"
+        echo "LOG: $LOG"
 
-        #comprueba que el numero de jugadores es 2,3 o 4
-        if [ $nuevo_jugadores -eq 2 ] || [ $nuevo_jugadores -eq 3 ] || [ $nuevo_jugadores -eq 4 ]
-        then
-            #DOCUMENTAR
-            sed "s/^JUGADORES=.*/JUGADORES=$nuevo_jugadores/" config.cfg 
-            clear
-            echo "JUGADORES actualizado a $nuevo_jugadores"
-
-        else
-            echo "Número de jugadores no válido. Debe ser 2, 3 o 4."
-        fi
-        ;;
-        2)
-            echo "ESTRATEGIA actual: $(grep '^ESTRATEGIA=' config.cfg)"
-            echo -n "Elegir nueva estrategia (0, 1 o 2): "
-            read nuevo_estrategia
-        
-        if [ $nuevo_estrategia -eq 0 ] || [ $nuevo_estrategia -eq 1 ] || [ $nuevo_estrategia -eq 2 ]
-        then
-            #DOCUMENTAR
-            sed "s/^ESTRATEGIA=.*/ESTRATEGIA=$nuevo_estrategia/" config.cfg 
-            clear
-            echo "ESTRATEGIA actualizada a $nuevo_estrategia"
-
-        else
-            echo "Número de estrategia no válido. Debe ser 0, 1 o 2."
-        fi
-            ;;
-        3)
-            echo -n "Seleccione la ruta donde se guardará o se encuentra el registro..."
-            read nuevo_directorio
-            if [ ! -d "$nuevo_directorio" ] #comprueba que el directorio existe
-            then
-                echo "ERROR: El directorio no existe o no es correcto."
-            else
-            #Usar como separador de directorios el caracter # para evitar conflictos con las barras
-            sed "s#^LOG=.*#LOG=$nuevo_directorio#" config.cfg
-            clear 
-            echo "LOG actualizado a $nuevo_directorio"
-            fi
-            ;;
-        4)
-            echo "JUGADORES actual: $(grep '^JUGADORES=' config.cfg)"
-            echo "ESTRATEGIA actual: $(grep '^ESTRATEGIA=' config.cfg)"
-            echo "RUTA actual: $(grep '^LOG=' config.cfg)"
-            read -p "Pulse una tecla para continuar..."
-            ;;
-        5)
-            break
-            ;;
-        *)
-            echo "Seleccione 1, 2, o 3."
-            ;;
-    esac
-done
-         
-}
-
-
-function Comprobaciones
-{
-while [ ! -f "config.cfg" ] || [ ! -s "config.cfg"  ]     #-f comprueba si es un archivo y -s si esta vacio
-do                
-        echo "El archivo de configuración 'config.cfg' no se ha encontrado en este directorio o esta vacio."
-        echo "Por favor, asegúrese de crear el archivo de configuración antes de ejecutar este juego."
-        echo -n "Pulse 1 para crear (Predeterminado) o 2 para salir..."
-        
+        echo "==============================="
+        echo "          CONFIGURACION        "
+        echo "==============================="
+        echo "1) NUMERO JUGADORES"
+        echo "2) SELECCIONAR ESTRATEGIA"
+        echo "3) SELECCIONAR LOG"
+        echo "4) VER CONFIGURACION"
+        echo "5) VOLVER"
+        echo "==============================="
+        echo -n "Introduzca una opción.......... "
         read opcion
 
         case $opcion in
-            1)  
-                clear
-                crearDefault
-                sleep 1          
-                ;;
-            2) 
-                echo "Saliendo del juego..."
-                sleep 1
-                exit 0
-                ;;
+            1)
+                echo "JUGADORES actuales: $(grep '^JUGADORES=' config.cfg)"
+                echo -n "Nuevo número de jugadores (2, 3 o 4): "
+                read nuevo_jugadores
 
-            *) echo "Seleccione 1 o 2."
+                if [ $nuevo_jugadores = 2 ] || [ $nuevo_jugadores = 3 ] || [ $nuevo_jugadores = 4 ]
+                then
+                    echo $nuevo_jugadores > config.cfg
+                    echo $estrategia >> config.cfg
+                    echo $log >> config.cfg
+                    echo "JUGADORES actualizado a $nuevo_jugadores"
+                else
+                    echo "Número de jugadores no válido. Debe ser 2, 3 o 4."
+                fi
+                ;;
+            2)
+                echo "ESTRATEGIA actual: $(grep '^ESTRATEGIA=' config.cfg)"
+                echo -n "Elegir nueva estrategia (0, 1 o 2): "
+                read nuevo_estrategia
+
+                if [ $nuevo_estrategia -eq 0 ] || [ $nuevo_estrategia -eq 1 ] || [ $nuevo_estrategia -eq 2 ]
+                then
+                    echo $jugadores > config.cfg
+                    echo $nuevo_estrategia >> config.cfg
+                    echo $log >> config.cfg
+                    echo "ESTRATEGIA actualizada a $nuevo_estrategia"
+                else
+                    echo "Número de estrategia no válido. Debe ser 0, 1 o 2."
+                fi
+                ;;
+            3)
+                echo -n "Seleccione la ruta donde se guardará o se encuentra el registro..."
+                read nuevo_directorio
+
+                if [ ! -d "$nuevo_directorio" ]
+                then
+                    echo "ERROR: El directorio no existe o no es correcto."
+                else
+                    echo $jugadores > config.cfg
+                    echo $estrategia >> config.cfg
+                    echo $nuevo_directorio >> config.cfg
+                    echo "LOG actualizado a $nuevo_directorio"
+                fi
+                ;;
+            4)
+                cat config.cfg
+                read -p "Pulse una tecla para continuar..."
+                ;;
+            5)
+                break
+                ;;
+            *)
+                echo "Seleccione 1, 2, 3, 4 o 5"
                 ;;
         esac
-    
-done
+    done
+}
 
-# El siguiente if comprueba que el fichero mantiene el formato esperado para su lectura posterior
-# Si no se cumple, se informa al usuario y crea uno predeterminado
+function Comprobaciones
+{
+    if [ ! -f "config.cfg" ] || [ ! -s "config.cfg" ]
+    then
+        echo "El archivo de configuración 'config.cfg' no se ha encontrado en este directorio o esta vacío."
+        echo "Por favor, asegúrese de crear el archivo de configuración antes de ejecutar este juego."
+        read opcion
+        exit 0
+    fi
 
-if   ! grep '^JUGADORES=[2-4]$' config.cfg  ||  ! grep '^ESTRATEGIA=[0-2]$' config.cfg  ||  ! grep -e '^LOG=.*' config.cfg 
-        then
-            clear
-            echo "ERROR: El archivo de configuración 'config.cfg' no es correcto, creando uno DEFAULT"
-            crearDefault
-fi
+    if ! grep '^JUGADORES=[2-4]$' config.cfg || ! grep '^ESTRATEGIA=[0-2]$' config.cfg || ! grep '^LOG=.*' config.cfg
+    then
+        clear
+        echo "ERROR: El archivo de configuración 'config.cfg' no es correcto"
+        exit 0
+    fi
 }
 
 if [ "$1" = "-g" ]
 then
     echo "      =========================="
-    echo "              DESCRIPCION         "
+    echo "              DESCRIPCION       "
     echo "      =========================="
     echo
     echo "               AUTORES"
@@ -168,42 +137,41 @@ then
     echo "              ESTRATEGIAS       "
     echo "      =========================="
     echo
-    echo "      Estrategia 0 Aleatorio: Los jugadores colocan automaticamente la primera carta que puedan colocar"
-    echo "      Estrategia 1 Mayores: Los jugadores colocan la carta de mayor valor que puedan colocar para reducir el numero de puntos restantes al final de la partida"
+    echo "      Estrategia 0 Aleatorio: Los jugadores colocan automáticamente la primera carta que puedan colocar"
+    echo "      Estrategia 1 Mayores: Los jugadores colocan la carta de mayor valor que puedan colocar para reducir el número de puntos restantes al final de la partida"
     echo "      Estrategia 2 ..."
-    echo 
-    #-p para que no haga salto de linea
+    echo
+
     read -p "Pulse una tecla para continuar..."
     exit 0
 fi
 
-Comprobaciones #Comprobamos que el archivo de configuracion existe y tiene el formato correcto
+Comprobaciones
 
 while true
 do
-    mostrarMenu #mostramos el menu
+    mostrarMenu
     read opcion
 
     case $opcion in
-        [Cc]) #caso c o C
+        [Cc])
             echo "ACCEDIENDO A CONFIGURACION..."
             configuracion
             ;;
-        [Jj]) #caso j o J
+        [Jj])
             echo "PREPARATE PARA JUGAR..."
             ;;
-        [Ee]) #caso e o E
+        [Ee])
             echo "ACCEDIENDO A ESTADISTICAS..."
             ;;
-        [Ff]) #caso f o F
+        [Ff])
             echo "ACCEDIENDO A CLASIFICACION..."
             ;;
-        [Ss]) #caso s o S
+        [Ss])
             exit 0
             ;;
-        *) echo "Opcion incorrecta"
+        *)
+            echo "Opcion incorrecta"
             ;;
     esac
 done
-
-
