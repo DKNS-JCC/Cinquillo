46979cbd222c8a8ada8a5683ca73781e57a00328
DKNS-Usal <jorgecuadradojcc@usal.es>
2023-10-28 14:03:07 +0200

    🔍 UI

--- cinquillo.sh
+++ cinquillo.sh
@@ -114,14 +114,14 @@ function Comprobaciones
         echo "El archivo de configuración 'config.cfg' no se ha encontrado en este directorio o esta vacío."
         echo "Por favor, asegúrese de crear el archivo de configuración antes de ejecutar este juego."
         read opcion
-        exit 0
+        exit -1
     fi
 
     if ! grep '^JUGADORES=[2-4]$' config.cfg || ! grep '^ESTRATEGIA=[0-2]$' config.cfg || ! grep '^LOG=*' config.cfg
     then
         clear
         echo "ERROR: El archivo de configuración 'config.cfg' no es correcto"
-        exit 0
+        exit -1
     fi
 }
 
@@ -154,111 +154,109 @@ function barajar {
   done
 }
 function repartir_por_jugadores {
-    # Crea dos arrays vacíos para los dos jugadores
-jugador1=()
-jugador2=()
-jugador3=()
-jugador4=()
+# Crea dos arrays vacíos para los dos jugadores
+JUGADOR1=()
+JUGADOR2=()
+JUGADOR3=()
+JUGADOR4=()
 
 # Calcula cuántas cartas se repartirán a cada jugador
-total_cartas=${#BARAJA[@]}
+TOTAL_CARTAS=${#BARAJA[@]}
 
 case $JUGADORES in
     2)
-        cartas_por_jugador=$((total_cartas / 2))
-        for ((i=0; i<cartas_por_jugador; i++)); do
+        CARTAS_POR_JUGADOR=$((TOTAL_CARTAS / 2))
+        for ((i=0; i<CARTAS_POR_JUGADOR; i++)); 
+        do
             # Añade la carta al jugador 1
-            jugador1+=("${BARAJA[i]}")
+            JUGADOR1+=("${BARAJA[i]}")
             # Añade la carta al jugador 2
-            jugador2+=("${BARAJA[i + cartas_por_jugador]}")
+            JUGADOR2+=("${BARAJA[i + CARTAS_POR_JUGADOR]}")
         done
-        echo "-------------------------"
-        echo "Cartas del Jugador 1:"
-        for CARTA in "${jugador1[@]}"; do
-            echo "$CARTA"
-        done
-        echo "-------------------------"
-        echo "Cartas del Jugador 2:"
-        for CARTA in "${jugador2[@]}"; do
-            echo "$CARTA"
+        echo "-----------------------------------------------------------------------------------------"
+        printf "%-30s %-30s\n" "Cartas del Jugador 1:" "Cartas del Jugador 2:"
+        echo
+        for ((i=0;i<CARTAS_POR_JUGADOR;i++)); 
+        do
+            printf "%-30s %-30s\n" "${JUGADOR1[i]}" "${JUGADOR2[i]}"
         done
         ;;
 
     3)
-        cartas_por_jugador=$((total_cartas / 3))
-        for ((i=0; i<cartas_por_jugador; i++)); do
+        CARTAS_POR_JUGADOR=$((TOTAL_CARTAS / 3))
+        for ((i=0; i<CARTAS_POR_JUGADOR; i++)); do
             # Añade la carta al jugador 1
-            jugador1+=("${BARAJA[i]}")
+            JUGADOR1+=("${BARAJA[i]}")
             # Añade la carta al jugador 2
-            jugador2+=("${BARAJA[i + cartas_por_jugador]}")
+            JUGADOR2+=("${BARAJA[i + CARTAS_POR_JUGADOR]}")
             # Añade la carta al jugador 3
-            jugador3+=("${BARAJA[i + cartas_por_jugador * 2]}")
+            JUGADOR3+=("${BARAJA[i + CARTAS_POR_JUGADOR * 2]}")
         done
-        jugador1+=("${BARAJA[cartas_por_jugador * 3]}")
-        echo "-------------------------"
-        echo "Cartas del Jugador 1:"
-        for CARTA in "${jugador1[@]}"; do
-            echo "$CARTA"
+        echo "-----------------------------------------------------------------------------------------"
+        printf "%-30s %-30s %-30s\n" "Cartas del Jugador 1:" "Cartas del Jugador 2:" "Cartas del Jugador 3:"
+        echo
+        for ((i=0;i<CARTAS_POR_JUGADOR;i++)); do
+            printf "%-30s %-30s %-30s\n" "${JUGADOR1[i]}" "${JUGADOR2[i]}" "${JUGADOR3[i]}"
         done
-        echo "-------------------------"
-        echo "Cartas del Jugador 2:"
-        for CARTA in "${jugador2[@]}"; do
-            echo "$CARTA"
-        done
-        echo "-------------------------"
-        echo "Cartas del Jugador 3:"
-        for CARTA in "${jugador3[@]}"; do
-            echo "$CARTA"
-        done
+
         ;;
 
     4)
-        cartas_por_jugador=$((total_cartas / 4))
-        for ((i=0; i<cartas_por_jugador; i++)); do
+        CARTAS_POR_JUGADOR=$((TOTAL_CARTAS / 4))
+        for ((i=0; i<CARTAS_POR_JUGADOR; i++)); 
+        do
             # Añade la carta al jugador 1
-            jugador1+=("${BARAJA[i]}")
+            JUGADOR1+=("${BARAJA[i]}")
             # Añade la carta al jugador 2
-            jugador2+=("${BARAJA[i + cartas_por_jugador]}")
+            JUGADOR2+=("${BARAJA[i + CARTAS_POR_JUGADOR]}")
             # Añade la carta al jugador 3
-            jugador3+=("${BARAJA[i + cartas_por_jugador * 2]}")
+            JUGADOR3+=("${BARAJA[i + CARTAS_POR_JUGADOR * 2]}")
             # Añade la carta al jugador 4
-            jugador4+=("${BARAJA[i + cartas_por_jugador * 3]}")
+            JUGADOR4+=("${BARAJA[i + CARTAS_POR_JUGADOR * 3]}")
         done
-        echo "-------------------------"
-        echo "Cartas del Jugador 1:"
-        for CARTA in "${jugador1[@]}"; do
-            echo "$CARTA"
-        done
-        echo "-------------------------"
-        echo "Cartas del Jugador 2:"
-        for CARTA in "${jugador2[@]}"; do
-            echo "$CARTA"
-        done
-        echo "-------------------------"
-        echo "Cartas del Jugador 3:"
-        for CARTA in "${jugador3[@]}"; do
-            echo "$CARTA"
-        done
-        echo "-------------------------"
-        echo "Cartas del Jugador 4:"
-        for CARTA in "${jugador4[@]}"; do
-            echo "$CARTA"
+        echo "-----------------------------------------------------------------------------------------"
+        printf "%-30s %-30s %-30s %-30s\n" "Cartas del Jugador 1:" "Cartas del Jugador 2:" "Cartas del Jugador 3:" "Cartas del Jugador 4:"
+        echo
+        for ((i=0;i<CARTAS_POR_JUGADOR;i++));
+        do
+            printf "%-30s %-30s %-30s %-30s\n" "${JUGADOR1[i]}" "${JUGADOR2[i]}" "${JUGADOR3[i]}" "${JUGADOR4[i]}"
         done
         ;;
 
     *)
-        echo "ERROR: El número de jugadores no es correcto"
-        exit 0
+        echo "ERROR: EL NÚMERO DE JUGADORES NO ES CORRECTO"
+        exit -1
         ;;
 esac
 
+# Declarar arrays de cartas de cada palo llenos
+BASTOS=("1 de bastos" "2 de bastos" "3 de bastos" "4 de bastos" "5 de bastos" "6 de bastos" "7 de bastos" "SOTA de bastos" "CABALLO de bastos" "REY de bastos")
+COPAS=("1 de copas" "2 de copas" "3 de copas" "4 de copas" "5 de copas" "6 de copas" "7 de copas" "SOTA de copas" "CABALLO de copas" "REY de copas")
+ESPADAS=("1 de espadas" "2 de espadas" "3 de espadas" "4 de espadas" "5 de espadas" "6 de espadas" "7 de espadas" "SOTA de espadas" "CABALLO de espadas" "REY de espadas")
+OROS=("1 de oros" "2 de oros" "3 de oros" "4 de oros" "5 de oros" "6 de oros" "7 de oros" "SOTA de oros" "CABALLO de oros" "REY de oros")
 }
+
+function mostrar_palos {
+    echo "-----------------------------------------------------------------------------------------"
+    for ((i = 0; i < 10; i++)); do
+        PALO_BASTO="${BASTOS[i]}"
+        PALO_COPA="${COPAS[i]}"
+        PALO_ESPADA="${ESPADAS[i]}"
+        PALO_ORO="${OROS[i]}"
+
+        printf "%-30s %-30s %-30s %-30s\n" "$PALO_BASTO" "$PALO_COPA" "$PALO_ESPADA" "$PALO_ORO"
+    done
+}
+
+
 
 function juego {
     crear_baraja
     barajar
     repartir_por_jugadores
-    
+    mostrar_palos
+
+
 
 }
 
@@ -310,7 +308,7 @@ do
             configuracion
             ;;
         [Jj])
-            echo "PREPARATE PARA JUGAR..."
+            clear
             juego
             ;;
         [Ee])
